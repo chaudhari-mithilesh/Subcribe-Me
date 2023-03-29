f41f9a88c043d20b5df1c0f65fe76af8880932df
chaudhari-mithilesh <mithilesh.chaudhari@wisdmlabs.com>
2023-03-29 10:01:14 +0530

    Initial Commit.

---
+++ includes/class-subscribe-me-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://mithilesh.wisdmlabs.net/
+ * @since      1.0.0
+ *
+ * @package    Subscribe_Me
+ * @subpackage Subscribe_Me/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Subscribe_Me
+ * @subpackage Subscribe_Me/includes
+ * @author     Mithilesh <mithilesh.chaudhaudhari@wisdmlabs.com>
+ */
+class Subscribe_Me_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
