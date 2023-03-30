93a0ad2164383010381be1ef67c5d22e95a792ed
chaudhari-mithilesh <mithilesh.chaudhari@wisdmlabs.com>
2023-03-30 22:47:34 +0530

    Added HTML tables for admin menu pages here.

--- admin/partials/subscribe-me-admin-display.php
+++ admin/partials/subscribe-me-admin-display.php
@@ -14,3 +14,55 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
+<?php
+
+function menu_page_html()
+{
+
+    // global $wpdb;
+    // $table_name = $wpdb->prefix . 'events';
+
+    // $results = $wpdb->get_results("SELECT * FROM $table_name WHERE date >= DATE( NOW() ) ORDER BY date");
+
+    $emails = get_option('my_sub_email', array());
+
+
+    echo '<center><table class="sm-table" border="1">
+    <thead>
+        <th>Subscriber Count</th>
+    </thead>
+    <tbody>
+        <tr>
+        <td>' . count($emails) . '</td></tr>
+    </tbody>
+</table></center>
+';
+
+    echo '<br><br>';
+
+    echo '
+    <div>
+    <table class="sm-table" border="1" cell-spacing="0">
+        <thead>
+            <th>Email IDs</th>
+        </thead>
+        <tbody>';
+    foreach ($emails as $record) {
+        echo '
+        <tr>
+            <td>' .
+            $record
+            . '
+        </td>
+        </tr>
+    ';
+    }
+
+    echo '</tbody>
+    </table>
+    </div>
+    ';
+}
+
+?>
